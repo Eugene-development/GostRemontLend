e640dcb1f5f98a1486c51b1b0f2c7943b29cba3c
indexpro24@gmail.com <indexpro24@gmail.com>
2021-11-12 20:08:15 +0300

    git init

--- src/store/leads/measurement/index.js
+++ src/store/leads/measurement/index.js
@@ -1,21 +1,49 @@
 export const state = () => ({
-  ruleMeasurement: {},
+  ruleMeasurementName: {},
+  ruleMeasurementPhone: {},
+  ruleMeasurementAddress: {},
+  ruleMeasurementTime: {},
+  ruleMeasurementComment: {},
 });
 
 
 export const actions = {
-  updateMeasurement({commit, state}, e) {
-    const ruleMeasurement = {measurement: e.target.value};
-    commit('UPDATE_MEASUREMENT', ruleMeasurement)
+  updateMeasurementName({commit, state}, e) {
+    const ruleMeasurementName = {measurementName: e.target.value};
+    commit('UPDATE_MEASUREMENT_NAME', ruleMeasurementName)
+  },
+  updateMeasurementPhone({commit, state}, e) {
+    const ruleMeasurementPhone = {measurementPhone: e.target.value};
+    commit('UPDATE_MEASUREMENT_PHONE', ruleMeasurementPhone)
+  },
+  updateMeasurementAddress({commit, state}, e) {
+    const ruleMeasurementAddress = {measurementAddress: e.target.value};
+    commit('UPDATE_MEASUREMENT_ADDRESS', ruleMeasurementAddress)
+  },
+  updateMeasurementTime({commit, state}, e) {
+    const ruleMeasurementTime = {measurementTime: e.target.value};
+    commit('UPDATE_MEASUREMENT_TIME', ruleMeasurementTime)
+  },
+  updateMeasurementComment({commit, state}, e) {
+    const ruleMeasurementComment = {measurementComment: e.target.value};
+    commit('UPDATE_MEASUREMENT_COMMENT', ruleMeasurementComment)
   }
 };
 
 
 export const mutations = {
-  UPDATE_MEASUREMENT: (state, ruleMeasurement) => state.ruleMeasurement = ruleMeasurement,
+  UPDATE_MEASUREMENT_NAME: (state, ruleMeasurementName) => state.ruleMeasurementName = ruleMeasurementName,
+  UPDATE_MEASUREMENT_PHONE: (state, ruleMeasurementPhone) => state.ruleMeasurementPhone = ruleMeasurementPhone,
+  UPDATE_MEASUREMENT_ADDRESS: (state, ruleMeasurementAddress) => state.ruleMeasurementAddress = ruleMeasurementAddress,
+  UPDATE_MEASUREMENT_TIME: (state, ruleMeasurementTime) => state.ruleMeasurementTime = ruleMeasurementTime,
+  UPDATE_MEASUREMENT_COMMENT: (state, ruleMeasurementComment) => state.ruleMeasurementComment = ruleMeasurementComment,
 };
 
 
 export const getters = {
-  ruleMeasurement: state => state.ruleMeasurement
+  ruleMeasurementName: state => state.ruleMeasurementName,
+  ruleMeasurementPhone: state => state.ruleMeasurementPhone,
+  ruleMeasurementAddress: state => state.ruleMeasurementAddress,
+  ruleMeasurementTime: state => state.ruleMeasurementTime,
+  ruleMeasurementComment: state => state.ruleMeasurementComment
 };
