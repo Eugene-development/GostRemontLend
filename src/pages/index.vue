d572914075f8a5d66a7c15295cd78bf04360ec28
indexpro24@gmail.com <indexpro24@gmail.com>
2021-11-17 20:28:03 +0300

    git init

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -597,12 +597,12 @@
 
           <!-- CTA Section -->
           <div class="relative bg-gray-900" id="anchor4">
-            <div class="relative h-56 bg-green-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+            <div class="relative h-56  sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
               <img class="w-full h-full object-cover"
                    src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100"
                    alt="">
               <div aria-hidden="true"
-                   class="absolute inset-0 bg-gradient-to-r from-cyan-200 to-cyan-400 mix-blend-multiply"></div>
+                   class="absolute inset-0 bg-gradient-to-r from-cyan-100 to-green-200 mix-blend-multiply"></div>
             </div>
             <div class="relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
               <div class="md:ml-auto md:w-1/2 md:pl-10">
